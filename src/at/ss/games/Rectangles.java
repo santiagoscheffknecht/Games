69b5b8b1543b8087de06862137f5c572a1ae7b96
santiago <santiago.scheffknecht@student.htldornbirn.at>
2023-05-02 11:06:50 +0200

    Aufgabe 03 beendet

--- src/at/ss/games/Rectangles.java
+++ src/at/ss/games/Rectangles.java
@@ -1,7 +1,6 @@
 package at.ss.games;
 
 import org.newdawn.slick.*;
-import org.newdawn.slick.tests.AntiAliasTest;
 
 public class Rectangles extends BasicGame {
     private float yr;
@@ -10,7 +9,9 @@ public class Rectangles extends BasicGame {
     private float xo;
     private float yc;
     private float xc;
-    private float speed;
+    private float directionr;
+    private float directiono;
+    private float directionc;
 
     public Rectangles(String title) {
         super(title);
@@ -22,11 +23,77 @@ public class Rectangles extends BasicGame {
         this.yr = 100;
         this.xo = 100;
         this.yc = 100;
+        this.directionr = 1;
+        this.directiono = 1;
+        this.directionc = 1;
     }
 
     @Override
     public void update(GameContainer gameContainer, int delta) throws SlickException {
-       
+
+        //rectagle
+        if (directionr == 1){
+            xr += delta;
+        }
+        else if(directionr == 2){
+            yr += delta;
+        }
+        else if(directionr == 3){
+            xr -= delta;
+        }
+        else if(directionr == 4){
+            yr-= delta;
+        }
+        else {
+            yr = 50;
+        }
+
+        if (xr == 500 && yr == 100){
+            directionr = 2;
+        }
+        else if(xr == 500 && yr == 500){
+            directionr = 3;
+        }
+        else if(xr == 100 && yr == 500){
+            directionr = 4;
+        }
+        else if(xr == 100 && yr == 100) {
+            directionr = 1;
+        }
+
+    //Oval
+        if (directiono == 1){
+            xo += delta;
+        }
+        else if (directiono == 2){
+            xo -= delta;
+        }
+
+        if (xo == 500){
+            directiono = 2;
+        }
+        else if (xo == 100){
+            directiono = 1;
+        }
+
+        //cyrcle
+        if (directionc == 1){
+            yc += delta;
+        }
+        else if (directionc == 2){
+            yc -= delta;
+        }
+
+        if (yc == 500){
+            directionc = 2;
+        }
+        else if (yc == 100){
+            directionc = 1;
+        }
+
+
+
+
 
     }
 
